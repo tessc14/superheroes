34473aaeb1af701f740a8a41da93a8777b68d605
tessc14 <tessc14@gmail.com>
2023-02-21 18:35:03 +0000

    change url format

--- heroes/src/components/HeroList/index.jsx
+++ heroes/src/components/HeroList/index.jsx
@@ -1,12 +1,14 @@
 import React, {useState, useEffect} from 'react'
 
+const url = "https://superheroapi.com/api/2075790332591226/search/superman"
+
 function HeroList(heroes) {
 
     const [hero, setHero] = useState('')
 
     useEffect(() => {
         async function getFact() {
-            const response = await fetch('https://superheroapi.com/api/2075790332591226/search/superman')
+            const response = await fetch(url)
             const data =  (await response.json())["data"][0]["attributes"]["body"]
             setHero(data)
         }
